a70188bf677f52c1760027bc5fb277a8c9752733
够立小激 <84974878+Goulixiaoji@users.noreply.github.com>
2023-01-31 22:18:28 +0800

    Add files via upload

---
+++ src/main/java/hibi/blahaj/mixin/MobEntityMixin.java
@@ -0,0 +1,30 @@
+package hibi.blahaj.mixin;
+
+import hibi.blahaj.CuddlyItem;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Environment(EnvType.CLIENT)
+@Mixin(MobEntity.class)
+public class MobEntityMixin {
+    @Inject(
+            method = "interactMob",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private void interactWithPlushInjector(PlayerEntity player, Hand hand,
+                                           CallbackInfoReturnable<ActionResult> cir) {
+        if (player.getStackInHand(hand).getItem() instanceof CuddlyItem) {
+            cir.setReturnValue(ActionResult.PASS);
+            cir.cancel();
+        }
+    }
+}
